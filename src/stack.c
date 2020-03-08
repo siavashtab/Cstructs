8bed2e3b75a42b7fd32e54e74d597fcc390db7be
Siavash Tabrizian <30736084+siavashtab@users.noreply.github.com>
2020-03-08 18:45:38 -0500

    stack subroutines on stack.c

--- src/stack.c
+++ src/stack.c
@@ -19,5 +19,60 @@
  #include <stack.h>
  
  
- 
+ int isempty() {
+
+   if(top == -1)
+      return 1;
+   else
+      return 0;
+}
+   
+int isfull() {
+
+   if(top == MAXSIZE)
+      return 1;
+   else
+      return 0;
+}
+
+int peek() {
+   return stack[top];
+}
+
+int pop() {
+   int data;
+	
+   if(!isempty()) {
+      data = stack[top];
+      top = top - 1;   
+      return data;
+   } else {
+      printf("Could not retrieve data, Stack is empty.\n");
+   }
+}
+
+int push(int data) {
+
+   if(!isfull()) {
+      top = top + 1;   
+      stack[top] = data;
+   } else {
+      printf("Could not insert data, Stack is full.\n");
+   }
+}
+
+
+#define INITIAL_CAPACITY 3
+
+void push(int *arr, int index, int value, int *size, int *capacity){
+     if(*size > *capacity){
+          realloc(arr, sizeof(arr) * 2);
+          *capacity = sizeof(arr) * 2;
+     }
+     
+     arr[index] = value;
+     *size = *size + 1;
+}
+
+
  
