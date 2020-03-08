0ef64db1239854a015ea29e3ee9108d1c99fd07c
Siavash Tabrizian <30736084+siavashtab@users.noreply.github.com>
2020-03-08 15:26:28 -0500

    newnode function

--- src/node_data.c
+++ src/node_data.c
@@ -13,3 +13,21 @@
  */
  
  
+#include <utils.h>
+#include <node_data.h>
+
+
+nodeType *newNode(nodeType *prev, nodeType *next, dVector nodedat)
+{
+	nodeType * new_node;
+	
+	if ( !(new_node = (nodeType *) mem_malloc(sizeof(nodeType))) )
+			errMsg("allocation", "newNode", "new_node", 0);
+	
+	if( !(new_node->prevnode = (nodeType *) mem_malloc (sizeof(nodeType))))
+			errMsg("allocation", "newNode", "previous node pointer", 0);
+	
+	if( !(new_node->nextnode = (nodeType *) mem_malloc (sizeof(nodeType))))
+			errMsg("allocation", "newNode", "next node pointer", 0);
+	
+}
--- src/node_data.h
+++ src/node_data.h
@@ -12,4 +12,25 @@
  */
  
  
+#ifndef NODE_DATA_H_
+#define NODE_DATA_H_
  
+ /*
+	nodeType is the main data structure that is going to be stored in each abstract formats
+	such as stack, queue, etc. 
+	It can contain different data formats, but the important feature of our design is that it contains pointers
+	to the previous entry and next entry.
+ */
+ typedef struct{
+	dVector  data;
+	
+	nodeType * prevnode;
+	nodeType * nextnode;
+}nodeType;
+
+
+/* subroutines in node_data.c */
+nodeType *newNode(nodeType *prev, nodeType *next, dVector nodedat);
+
+
+#endif /* node_data_H_ */
