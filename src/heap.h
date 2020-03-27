3b59dd51c8dd623f2cf83d0c4fb16006c6556bd3
Siavash Tabrizian <30736084+siavashtab@users.noreply.github.com>
2020-03-27 18:49:27 -0400

    heap some name updates in heap.h

--- src/heap.h
+++ src/heap.h
@@ -1,69 +1,74 @@
-//
-///*=====================================================\
-//* heap.h
-//*
-//*   C implementation of the sack abstract data type
-//*          using doubly linked list
-//*
-//*
-//*
-//*
-//*
-//*
-//*
-//*  Created on: March 8, 2020
-//*      Author: Siavash Tabrizian
-//*=====================================================*/
-// 
-//#ifndef HEAP_H_
-//#define HEAP_H_
-// 
-//#include <stdio.h>
-//#include <node_data.h>
-//
-//#define INITIAL_HeapSize 3
-// 
-//
-//
-///*
-//  Stack subroutines:
-//*/
-//
-///*
-//   newStack is the stack  constructor 
-//*/
-//int newHeap();
-//
-///*
-//isStackempty checks if a stack is empty or not
-//*/
-//int isHeapempty();
-//
-///*
-//isStackfull checks if a stack is full of not based on the stacksize
-//*/
-//int isHeapfull();
-//
-///*
-//Stackpeek returns the top node of the stack
-//*/
-//nodeType * Heappeek();
-//
-///*
-//Stackpop returns the top node of the stack and remove it from the stack
-//*/
-//nodeType * Heappop();
-//
-///*
-//insert a new node to the stack without changing the size of the stack
-//*/
-//int Heappush(nodeType *node);
-//
-///*
-//insert a new node to the stack with dynamically adjucting the size of the stack
-//*/
-//int HeappushDP(nodeType *node);
-//
-//
-//
-//#endif /* heap_H_ */
+
+/*=====================================================\
+* heap.h
+*
+*   C implementation of the sack abstract data type
+*          using doubly linked list
+*
+*
+*
+*
+*
+*
+*
+*  Created on: March 8, 2020
+*      Author: Siavash Tabrizian
+*=====================================================*/
+ 
+#ifndef HEAP_H_
+#define HEAP_H_
+ 
+#include <stdio.h>
+#include <node_data.h>
+
+#define INITIAL_HeapSize 3
+ 
+
+
+/*
+  HEAP subroutines:
+*/
+
+/*
+   newStack is the heap  constructor 
+*/
+int newHeap();
+
+/*
+isStackempty checks if a heap is empty or not
+*/
+int isHeapempty();
+
+/*
+isStackfull checks if a heap is full of not based on the stacksize
+*/
+int isHeapfull();
+
+/*
+Stackpeek returns the top node of the heap
+*/
+nodeType * Heappeek();
+
+/*
+Stackpop returns the top node of the heap and remove it from the heap
+*/
+nodeType * Heappop();
+
+/*
+insert a new node to the heap without changing the size of the heap
+*/
+int Heappush(nodeType *node);
+
+/*
+insert a new node to the heap in a predefined posititon (int pos)
+*/
+int Heapinsert(nodeType *node, int pos);
+
+/*
+insert a new node to the heap with dynamically adjucting the size of the heap
+*/
+int HeappushDP(nodeType *node);
+
+
+
+#endif /* heap_H_ */
